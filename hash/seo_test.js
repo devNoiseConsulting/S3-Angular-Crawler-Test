50853a5f6d117af0a7d11f41a2723eea2441976b
Michael J. Flynn <flynnmj@devnoise.com>
2014-10-14 16:20:48 -0400

    First commit of the app states that I want to test.

---
+++ hash/seo_test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var seoTest = angular.module('seo_test', [
+	'ngRoute'
+]);
+
+seoTest.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/default.html',
+			controller: 'DefaultCtrl'
+		})
+		.when('/test', {
+			templateUrl: 'views/test.html',
+			controller: 'DefaultCtrl'
+		})
+		.when('/fail', {
+			templateUrl: 'views/fail.html',
+			controller: 'DefaultCtrl'
+		})
+
+	.otherwise({redirectTo: '/'});
+});
+
+seoTest.controller('NavigationCtrl', function ($scope) {
+});
+
+seoTest.controller('DefaultCtrl', function($scope){
+});
---
+++ pushBang/seo_test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var seoTest = angular.module('seo_test', [
+	'ngRoute'
+]);
+
+seoTest.config(function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+	$locationProvider.hashPrefix('!');
+
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/default.html',
+			controller: 'DefaultCtrl'
+		})
+		.when('/test', {
+			templateUrl: 'views/test.html',
+			controller: 'DefaultCtrl'
+		})
+		.when('/fail', {
+			templateUrl: 'views/fail.html',
+			controller: 'DefaultCtrl'
+		})
+
+	.otherwise({redirectTo: '/'});
+});
+
+seoTest.controller('NavigationCtrl', function ($scope) {
+});
+
+seoTest.controller('DefaultCtrl', function($scope){
+});
